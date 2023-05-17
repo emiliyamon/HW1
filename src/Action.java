a51ec600a9a6e75c02286f63e6edf19ca883d1fd
Shakedha <shaked.hadad@campus.technion.ac.il>
2023-05-17 15:27:09 +0300

    AD MATAY

--- src/Action.java
+++ src/Action.java
@@ -8,10 +8,11 @@ public class Action {
     }
 
     public String toString () {
-        String move = "Move ";
-        String tileString = String.valueOf(tile);
-        String directionString = String.valueOf(direction);
+        //String move = "Move ";
+        //String tileString = String.valueOf(tile);
+        //String directionString = String.valueOf(direction);
 
-        return move + " " + tileString + " " + directionString;
+        //return move + " " + tileString + " " + directionString;
+        return "move" + " " + tile + " " + direction;
     }
 }
