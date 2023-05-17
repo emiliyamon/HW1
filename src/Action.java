150cdcaed3b7bca70f6fba3afb596c3813f9e777
emiliyamon <emiliyam@campus.technion.ac.il>
2023-05-17 13:56:10 +0300

    17052023 1355 added action class

--- src/Action.java
+++ src/Action.java
@@ -1,2 +1,18 @@
 public class Action {
+    EnumDirections direction;
+    Tile tile;
+
+    Action(EnumDirections direction, Tile tile) {
+        this.direction = direction;
+        this.tile = tile;
+    }
+
+    public String toString () {
+        String move = "Move ";
+        String tileString = String.valueOf(tile);
+        String directionString = String.valueOf(direction);
+        String returnString = move + " " + tileString + " " + directionString;
+
+        return returnString;
+    }
 }
