b3761dd7b29ec3ebf87ff694835d57bd30d69e0e
emiliyamon <emiliyam@campus.technion.ac.il>
2023-05-18 17:58:56 +0300

    18052023 1758 added heuristicValue class

---
+++ src/HeuristicValue.java
@@ -0,0 +1,11 @@
+public class HeuristicValue {
+    int value;
+
+    HeuristicValue(int value) {
+        this.value = 0;
+    }
+
+    public static void setValue(int value) {
+        this.value = value;
+    }
+}
