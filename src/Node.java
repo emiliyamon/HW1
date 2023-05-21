2deca1f3989cfe6e71cbdd6cb1fadd6b6c661898
emiliyamon <emiliyam@campus.technion.ac.il>
2023-05-21 18:48:46 +0300

    21052023 1848 98 problems but null in action aint one

--- src/Node.java
+++ src/Node.java
@@ -24,12 +24,12 @@ public class Node {
 
 
     public Node[] expand() {
-        Action[] possibleActions = state.actions();
+        Action[] possibleActions = this.state.actions();
         Node[] children = new Node[possibleActions.length];
         int i = 0;
         for (Action action : possibleActions) {
-            State newState = state.result(action);
-            Action newAction = new Action(preAction.direction, preAction.tile);
+            State newState = this.state.result(action);
+            Action newAction = new Action(action.direction, action.tile);
             Node child = new Node(newState, this, newAction);
             children[i] = child;
             i++;
