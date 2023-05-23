25d54ba6937659b61ad0e42637177be15317f9d7
emiliyamon <emiliyam@campus.technion.ac.il>
2023-05-23 17:51:42 +0300

    23052023 1751 working!!!!! with heuristic that doesnt make it work faster

--- src/HeuristicValue.java
+++ src/HeuristicValue.java
@@ -1,16 +1,27 @@
 public class HeuristicValue {
-    private Board board;
     private int heuristic;
 
     public HeuristicValue(Board board) {
-        this.board = board;
-        this.heuristic = 0;
+        heuristic = 0;
+
+        int rows = board.getTiles().length;
+        int cols = board.getTiles()[0].length;
+        int size = rows * cols;
+
+        for (int i = 0; i < board.getTiles().length; i++) {
+            for (int j = 0; j < board.getTiles()[0].length; j++) {
+                int tileValue = board.getTiles()[i][j].getValue();
+
+                if (tileValue != -1) {
+                    int targetRow = (tileValue - 1) / size;
+                    int targetCol = (tileValue - 1) % size;
+                    int distance = Math.abs(i - targetRow) + Math.abs(j - targetCol);
+                    heuristic += distance;
+                }
+            }
+        }
     }
 
-
-    public Board getBoard() {
-        return board;
-    }
 
     public int getHeuristic() {
         return heuristic;
